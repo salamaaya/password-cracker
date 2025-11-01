434632e06fb79a38c91dfd6b7f84144667e7c289
Aya Salama <asalama0204@gmail.com>
2025-11-01 15:28:32 -0400

    reverse brute force

--- dictionary.cpp
+++ dictionary.cpp
@@ -1,11 +1,12 @@
 #include <chrono>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
 
 using namespace std;
 
-#define DICTIONARY "data/rockyou.txt"
+#define DICTIONARY "rockyou.txt"
 #define NUM_PASSWORDS 14344391 /* the number of lines in the rockyou dataset */
 #define TERMINAL_WIDTH 80 /* used for progress bar, assume default width */
 
@@ -34,8 +35,14 @@ using namespace std;
  *  - 1: password found
  */
 int
-find_password_add_int(const string start, const size_t len, const string password)
+find_password_add_int(const string start, const int max_len, const string password)
 {
+    size_t len = start.length();
+
+    if ((int)len > max_len) {
+        return 0;
+    }
+
     for (size_t i = 0; i <= len; i++) {
         for (int j = 0; j < 10; j++) {
             string check_pass = start.substr(0, i);
@@ -46,14 +53,33 @@ find_password_add_int(const string start, const size_t len, const string passwor
                 cout << endl << "Your password is: " << check_pass << endl;
                 return 1;
             }
+
+            find_password_add_int(check_pass, max_len, password);
         }
     }
 
    return 0;
 }
 
+/*
+ * tries to find the correct password by reversing a common password. 
+ * return values:
+ *  - 0: password was NOT found
+ *  - 1: password found
+ */
 int
-find_password(const string password)
+find_password_reverse(const string start, const string password)
+{
+    string reversed = string(start.rbegin(), start.rend());
+
+    if (reversed == password) {
+        return 1;
+    }
+    return 0;
+}
+
+int
+find_password(const string password, const int max_len)
 {
     ifstream common_passwords(DICTIONARY);
     string curr_pass;
@@ -62,7 +88,6 @@ find_password(const string password)
     int progress = NUM_PASSWORDS / TERMINAL_WIDTH;
 
     cout << "Looking up password... " << endl;
-    cout << "Dictionary attack progress:" << endl;
 
     while (common_passwords >> curr_pass) {
         if (curr_pass == password) {
@@ -70,6 +95,9 @@ find_password(const string password)
         }
 
         if (++counter % progress == 0) {
+            if (counter ==  progress) {
+                cout << "Dictionary attack progress:" << endl;
+            }
             cout << unitbuf;
             cout << "#";
         }
@@ -85,8 +113,14 @@ find_password(const string password)
     counter = 0;
 
     while (common_passwords >> curr_pass) {
-        if (find_password_add_int(curr_pass, curr_pass.length(), password)) {
+        /* each if statement will be assigned to a node */
+        if (find_password_add_int(curr_pass, max_len, password)) {
             cout << endl << "Found the password by adding numbers!" << endl;
+            return 1;
+        }
+
+        if (find_password_reverse(curr_pass, password)) {
+            cout << endl << "Found the password by reversing a common password!" << endl;
             return 1;
         }
 
@@ -102,30 +136,33 @@ find_password(const string password)
 int
 main(int argc, char* argv[])
 {
-    if (argc != 2) {
-        cerr << "Usage: " << argv[0] << " <password>" << endl;
+    if (argc < 2 || argc > 3) {
+        cerr << "Usage: " << argv[0] << " <password> [maximum length]" << endl;
         return 1;
     }
 
-    // unordered_set<string> dictionary;
     string password = argv[1];
+    int max_len = 128;
+    if (argc == 3) {
+        istringstream iss;
+        iss.str(argv[2]);
 
-    // cout << "Creating dictionary..." << endl;
-    // time(&begin);
-    // dictionary = create_dictionary();
-    // time(&end);
+        if (!(iss >> max_len)) {
+            cerr << "Error: Second argument must be a size_t." << endl;
+            return 1;
+        }
+        if (max_len < 0) {
+            cerr << "Error: Second argument must be greater than 0." << endl;
+            return 1;
+        }
+        
+    } 
 
-    // cout << "Dictionary created in " << difftime(end, begin) << " seconds." << endl;
-
-    // cout << "Dictionary contains " << dictionary.size() << " passwords." << endl;
-
-    // time(&begin);
     auto start = chrono::high_resolution_clock::now();
-    if (!find_password(password)) {
+    if (!find_password(password, max_len)) {
         cout << "Nice password, we couldn't crack it!" << endl;
     }
 
-    // time(&end);
     auto end = chrono::high_resolution_clock::now();
     double time_taken = 
       chrono::duration_cast<chrono::nanoseconds>(end - start).count() * 1e-9;
