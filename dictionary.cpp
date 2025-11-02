3c036e53ea63f9c360fb62d669bf7e492861d5fa
Aya Salama <asalama0204@gmail.com>
2025-11-01 22:45:56 -0400

    implemented repeated password and optimized numbers
    remove cout

--- dictionary.cpp
+++ dictionary.cpp
@@ -3,33 +3,19 @@
 #include <fstream>
 #include <sstream>
 #include <string.h>
+#include <unordered_set>
 
 using namespace std;
 
 #define DICTIONARY "rockyou.txt"
 #define NUM_PASSWORDS 14344391 /* the number of lines in the rockyou dataset */
-#define TERMINAL_WIDTH 80 /* used for progress bar, assume default width */
+#define TERMINAL_WIDTH 80      /* used for progress bar, assume default width */
 
-/* 
-* rules to apply for finding variations (each node can be assigned a rule):
-* 1. adding numbers
-*   - password -> 1password -> password1 -> password2 -> ...
-* 2. reverse spelling
-*  - password -> drowssap
-* 3. repeating password twice
-*  - password -> passwordpassword
-* 4. changing casing
-*  - password -> Password -> PASSWORD -> paSSword -> ...
-* 5. leet spellings
-*  - password -> p4ssw0rd -> p@ssword -> ...
-* 7. deleting characters
-*  - password -> passwrd -> assword -> ...
-* 8. adding all prefixes and suffixes
-*  - password -> apassword -> passwordx -> xpasswordy -> ...
-*/
+unordered_set<string> tried_passwords; /* used for memoization */
 
 /*
- * tries to find the correct password by appending numbers to the base password
+ * tries to find the correct password by pre/appending numbers to the base 
+ * password (start)
  * return values:
  *  - 0: password was NOT found
  *  - 1: password found
@@ -39,30 +25,49 @@ find_password_add_int(const string start, const int max_len, const string passwo
 {
     size_t len = start.length();
 
-    if ((int)len > max_len) {
+    if (start == password) {
+        cout << endl
+             << "Your password is: " << start << endl;
+        return 1;
+    }
+
+    if ((int)len >= max_len) {
         return 0;
     }
 
-    for (size_t i = 0; i <= len; i++) {
+    for (size_t i = 0; i < len; i++) {
         for (int j = 0; j < 10; j++) {
-            string check_pass = start.substr(0, i);
-            check_pass += to_string(j);
-            check_pass += start.substr(i, len - i);
+            string prepend = to_string(j);
+            string append = start;
 
-            if (check_pass == password) {
-                cout << endl << "Your password is: " << check_pass << endl;
-                return 1;
+            prepend += start;
+            append += to_string(j);
+
+            /* memoize! */
+            if (tried_passwords.find(prepend) != tried_passwords.end()) {
+                return 0;
+            } else {
+                tried_passwords.insert(prepend);
             }
 
-            find_password_add_int(check_pass, max_len, password);
+            if (tried_passwords.find(append) != tried_passwords.end()) {
+                return 0;
+            } else {
+                tried_passwords.insert(append);
+            }
+
+            if (find_password_add_int(append, max_len, password)
+                || find_password_add_int(prepend, max_len, password)) {
+                    return 1;
+            }
         }
     }
 
-   return 0;
+    return 0;
 }
 
 /*
- * tries to find the correct password by reversing a common password. 
+ * tries to find the correct password by reversing a common password.
  * return values:
  *  - 0: password was NOT found
  *  - 1: password found
@@ -78,10 +83,50 @@ find_password_reverse(const string start, const string password)
     return 0;
 }
 
+/*
+ * tries to find the correct password by repeating a common password twice.
+ * return values:
+ *  - 0: password was NOT found
+ *  - 1: password found
+ */
+int
+find_password_repeat(const string password, const int max_len)
+{
+    string repeated = password + password;
+    size_t len = repeated.length();
+
+    if ((int)len > max_len) {
+        return 0;
+    }
+
+    if (repeated == password) {
+        return 1;
+    }
+    return 0;
+}
+
+/* tries to find the correct password by looking it up in the dictionary and then
+ * applying brute force variations such as the following:
+ *  rules to apply for finding variations (each node can be assigned a rule):
+ *  1. adding numbers
+ *     Example: password -> 1password -> password1 -> password2 -> ...
+ *  2. reverse spelling
+ *      Example: password -> drowssap
+ *  3. repeating password twice
+ *      Example: password -> passwordpassword
+ *  4. changing casing
+ *      Example: password -> Password -> PASSWORD -> paSSword -> ...
+ *  5. leet spellings
+ *      Example: password -> p4ssw0rd -> p@ssword -> ...
+ *  7. deleting characters
+ *      Example: password -> passwrd -> assword -> ...
+ *  8. adding all prefixes and suffixes
+ *      Example: password -> apassword -> passwordx -> xpasswordy -> ...
+ */
 int
 find_password(const string password, const int max_len)
 {
-    ifstream common_passwords(DICTIONARY);
+    ifstream common_password(DICTIONARY);
     string curr_pass;
     int counter = 0;
     /* number of passwords needed to print each '#' to keep track of progress */
@@ -89,13 +134,15 @@ find_password(const string password, const int max_len)
 
     cout << "Looking up password... " << endl;
 
-    while (common_passwords >> curr_pass) {
+    while (common_password >> curr_pass) {
         if (curr_pass == password) {
+            cout << endl
+                 << "Your password is: " << curr_pass << endl;
             return 1;
         }
 
         if (++counter % progress == 0) {
-            if (counter ==  progress) {
+            if (counter == progress) {
                 cout << "Dictionary attack progress:" << endl;
             }
             cout << unitbuf;
@@ -106,21 +153,29 @@ find_password(const string password, const int max_len)
     cout << ">" << endl;
 
     /* BRUTE FORCE attack starts here */
-    common_passwords.clear();
-    common_passwords.seekg(0, ios::beg);
+    common_password.clear();
+    common_password.seekg(0, ios::beg);
     cout << "Your password is not that common..." << endl;
-    cout << "Starting a brute force attack" << endl;
+    cout << "Starting a brute force attack..." << endl;
     counter = 0;
 
-    while (common_passwords >> curr_pass) {
+    while (common_password >> curr_pass) {
         /* each if statement will be assigned to a node */
         if (find_password_add_int(curr_pass, max_len, password)) {
-            cout << endl << "Found the password by adding numbers!" << endl;
+            cout << endl
+                 << "Found the password by adding numbers!" << endl;
             return 1;
         }
 
         if (find_password_reverse(curr_pass, password)) {
-            cout << endl << "Found the password by reversing a common password!" << endl;
+            cout << endl
+                 << "Found the password by reversing a common password!" << endl;
+            return 1;
+        }
+
+        if (find_password_repeat(curr_pass, max_len)) {
+            cout << endl
+                 << "Found the password by reversing a common password!" << endl;
             return 1;
         }
 
@@ -134,7 +189,7 @@ find_password(const string password, const int max_len)
 }
 
 int
-main(int argc, char* argv[])
+main(int argc, char *argv[])
 {
     if (argc < 2 || argc > 3) {
         cerr << "Usage: " << argv[0] << " <password> [maximum length]" << endl;
@@ -155,17 +210,16 @@ main(int argc, char* argv[])
             cerr << "Error: Second argument must be greater than 0." << endl;
             return 1;
         }
-        
-    } 
+    }
 
     auto start = chrono::high_resolution_clock::now();
     if (!find_password(password, max_len)) {
-        cout << "Nice password, we couldn't crack it!" << endl;
+        cout << "Nice password, couldn't crack it!" << endl;
     }
 
     auto end = chrono::high_resolution_clock::now();
-    double time_taken = 
-      chrono::duration_cast<chrono::nanoseconds>(end - start).count() * 1e-9;
+    double time_taken =
+        chrono::duration_cast<chrono::nanoseconds>(end - start).count() * 1e-9;
 
     cout << "Password lookup completed in " << time_taken << " seconds." << endl;
 
